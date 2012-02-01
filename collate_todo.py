45ebab8e24f7f4fdcde23288cdddc9802d53c246
brendanarnold <none@none>
2012-02-01 13:50:54 +0000

    Added \TODO macro

--- collate_todo.py
+++ collate_todo.py
@@ -11,11 +11,10 @@ with open('todo.txt', 'w') as out_fh:
                 line_num = 0
                 for line in fh:
                     line_num += 1
-                    if 'TODO' not in line:
+                    if '\\TODO' not in line:
                         continue
-                    if file_line is not None:                        
-                        out_fh.write(file_line)
-                        file_line = None
+                    out_fh.write(file_line)
+                    file_line = ''
                     out_fh.write('Line: %d\n%s\n'  % (line_num, line))
 print 'Done'
                     
